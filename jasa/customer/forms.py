1b489acd934b773bfdb45bf8c7b1dfdb8357b7e7
adi-em <adimurdiono@student.unu-jogja.ac.id>
2021-01-08 15:44:54 +0700

    update

--- jasa/customer/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from adminapp.models import dataguru, datamurid
-
-class imgform(forms.ModelForm):
-    class Meta():
-         model = datamurid
-         fields = ('__all__')
